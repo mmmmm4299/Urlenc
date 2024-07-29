65383d528bd47f0e2536d69a49321e4995589e25
mmmmm4299 <119816977+mmmmm4299@users.noreply.github.com>
2024-07-29 12:23:53 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+function convertUrl() {
+    const input = document.getElementById('urlInput').value;
+    let hexUrl = '';
+    for (let i = 0; i < input.length; i++) {
+        hexUrl += '%' + input.charCodeAt(i).toString(16).padStart(2, '0');
+    }
+
+    const isHttps = input.startsWith('https://');
+    const basePath = 'https://mmmmm4299.github.io/Urlenc/';
+    const path = isHttps ? 'hps/' : 'hp/';
+    const resultUrl = basePath + path + hexUrl;
+
+    document.getElementById('hexOutput').innerHTML = `
+        <a href="${resultUrl}" target="_blank">${resultUrl}</a>
+        <br>
+        <a href="${input}" target="_blank">Original URL</a>
+    `;
+}
